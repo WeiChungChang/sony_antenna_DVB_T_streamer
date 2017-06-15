6815300044e04ca7047b3c04dede75f032a25dd3
john <r97922153@gmail.com>
2017-06-15 14:22:52 +0800

    add claims & remove log

--- threadqueue.c
+++ threadqueue.c
@@ -1,3 +1,13 @@
+/* Copyright 2017-2018 Noovo Crop.
+ * This software it the property of Noovo Crop.
+ * You have to accept the terms in the license file before use
+ *
+ * Created by JohnChang      2017-06-12
+ * Modified by 
+ *
+ * Copyright (c) 2017-2018 Noovo Crop.  All rights reserved
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
